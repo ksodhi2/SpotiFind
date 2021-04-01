7434c31034f673cf07362763112111fa205ba4a5
Daniel Marks <danielpresliermarks@gmail.com>
2021-04-01 14:38:43 -0500

    initializing git repo

---
+++ Front-End/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+//import { Button } from '@material-ui/core';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <h1>SpotiFind!</h1>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
