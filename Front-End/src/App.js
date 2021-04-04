e87ed3f2f54a1ee57ea2fa30c2058a8fd6505d01
Daniel Marks <danielpresliermarks@gmail.com>
2021-04-04 16:34:59 -0500

    added responsive button

--- Front-End/src/App.js
+++ Front-End/src/App.js
@@ -1,15 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 //import { Button } from '@material-ui/core';
 
 
 function App() {
+
+  const [text, setText] = useState("No text.")
+
+  const updateText = () => {
+    setText("All The Single Ladies");
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         
         <h1>SpotiFind!</h1>
-        
+        <button onClick={() => updateText()}>Find tracks!</button>
+        <p>{text}</p>
       </header>
     </div>
   );
