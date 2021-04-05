4d14b6c8aea71f27d4885ba1cf781eb2ee660176
Daniel Marks <danielpresliermarks@gmail.com>
2021-04-04 23:55:39 -0500

    added backend server

--- Front-End/src/App.js
+++ Front-End/src/App.js
@@ -1,23 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
-import {useState} from "react";
+import React, {useState} from "react";
+import Axios from 'axios';
 //import { Button } from '@material-ui/core';
 
 
 function App() {
 
+  const [input, setInput] = useState("");
   const [text, setText] = useState("No text.")
 
   const updateText = () => {
     setText("All The Single Ladies");
   }
 
+  const exampleQuery = () => {
+    console.log("Querying");
+    Axios.get('https://localhost:8080/api/get').then((response) => {
+      alert('success');
+    })
+  }
+
+  
+
   return (
     <div className="App">
       <header className="App-header">
         
         <h1>SpotiFind!</h1>
-        <button onClick={() => updateText()}>Find tracks!</button>
+        <input></input>
+        <button onClick={() => exampleQuery()}>Find tracks!</button>
         <p>{text}</p>
       </header>
     </div>
