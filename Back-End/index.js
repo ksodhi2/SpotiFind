557006942edbd2d61c891cab1a9d6719000b5c74
Karan Sodhi <karan@Karans-MacBook-Pro.local>
2021-04-12 12:52:16 -0400

    added crud operations

--- Back-End/index.js
+++ Back-End/index.js
@@ -6,8 +6,8 @@ const bodyParser = require("body-parser");
 
 var db = mysql.createConnection({
     host: '34.121.38.95',
-    user: 'root',
-    password: 'CS411Team80',
+    user: 'karan',
+    password: 'karan123',
     database: 'spotifind'
 })
 
@@ -49,17 +49,71 @@ app.post("/api/addUserTopTracks", function(req, res){
 
 */
 
-app.get("/api/getArtistInfo", function(req, res){
+app.get("/api/getTrackInfo", function(req, res){
     // Fetch parameters from the call header
-    const artist = req.query.artist;
+    const track = req.query.track;
     // Generate SQL call
-    const sqlReq = `SELECT * FROM Artists WHERE artist_name = "${artist}";`;
+    const sqlReq = `SELECT * FROM Tracks WHERE track_name = "${track}";`;
     // Send call to the database 
     db.query(sqlReq, (err,result) => {
         // Send the SQL result in the API response
         res.send(result);
     })
 } )
+
+
+app.get("/api/getCountryTopTracks", function(req, res){
+    // Fetch parameters from the call header
+    const country = req.query.country;
+    // Generate SQL call
+    const sqlReq = `SELECT country, track_name, Track_Count FROM ( SELECT country, track_id, Track_Count, row_number() over (partition by country order by Track_Count DESC) as rnk FROM ( SELECT country, track_id , COUNT(track_id) as Track_Count FROM Users JOIN Top_Tracks ON Users.user_id = Top_Tracks.user_id GROUP BY country, track_id ) temp ) Qry JOIN Tracks on Qry.Track_id = Tracks.track_id where rnk <= 3 AND country = "${country}";`
+    // Send call to the database 
+    db.query(sqlReq, (err,result) => {
+        // Send the SQL result in the API response
+        res.send(result);
+    })
+} )
+
+app.get("/api/getUserTop", function(req, res){
+    // Fetch parameters from the call header
+    const userId = req.query.userId;
+    // Generate SQL call
+    const sqlReq = `SELECT * FROM Top_Tracks WHERE user_id = "${userId}" ORDER BY track_rank;`;
+    // Send call to the database 
+    db.query(sqlReq, (err,result) => {
+        // Send the SQL result in the API response
+        res.send(result);
+    })
+} )
+
+app.get("/api/insertTopTrack", function(req, res){
+    const userId = req.query.userId;
+    const trackId = req.query.trackId;
+    const trackRank = req.query.trackRank;
+    const sqlInsert = `INSERT INTO Top_Tracks (user_id, track_id, track_rank) values ("${userId}", "${trackId}", "${trackRank}");`;
+    db.query(sqlInsert, (err, result) => {
+        res.send(result);
+    })
+})
+
+app.get("/api/deleteTopTrack", function(req, res){
+    const userId = req.query.userId;
+    const trackId = req.query.trackId;
+    const sqlDelete = `DELETE FROM Top_Tracks WHERE user_id = "${userId}" AND track_id = "${trackId}";`;
+    db.query(sqlDelete, (err, result) => {
+        res.send(result);
+    })
+})
+
+app.get("/api/updateTrackRanking", function(req, res){
+    const userId = req.query.userId;
+    const trackId = req.query.trackId;
+    const trackRank = req.query.trackRank;
+    const sqlUpdate = `UPDATE Top_Tracks SET track_rank = "${trackRank}" WHERE user_id = "${userId}" AND track_id = "${trackId}";`;
+    db.query(sqlUpdate, (err, result) => {
+        res.send(result);
+    })
+})
 
 
 app.listen(8080, () => {
--- Front-End/src/App.js
+++ Front-End/src/App.js
@@ -9,20 +9,35 @@ function App() {
   const [input1, setInput1] = useState("");
   const [input2, setInput2] = useState("");
   const [input3, setInput3] = useState("");
+
   const [input4, setInput4] = useState("");
+  const [input5, setInput5] = useState("");
+  const [input6, setInput6] = useState("");
+
+  const [input7, setInput7] = useState("");
+  const [input8, setInput8] = useState("");
+
+  const [input9, setInput9] = useState("");
+  const [input10, setInput10] = useState("");
+  const [input11, setInput11] = useState("");
+
+
   const [text1, setText1] = useState("No query.")
   const [text2, setText2] = useState("No query.")
   const [text3, setText3] = useState("No query.")
   const [text4, setText4] = useState("No query.")
+  const [text5, setText5] = useState("No query.")
+  const [text6, setText6] = useState("No query.")
+
 
 
   const exampleQuery1 = () => {
     
     //Make a GET call to the backend server
-    Axios.get("http://localhost:8080/api/getArtistInfo", {
-      //Passing in the artist as the input parameter
+    Axios.get("http://localhost:8080/api/getTrackInfo", {
+      //Passing in the track as the input parameter
       params:{
-        artist: input1
+        track: input1
       }
     }).catch(function (error) {   //Catch any error from the response
       console.log(error);
@@ -39,17 +54,104 @@ function App() {
 
   //Put the second query here
   const exampleQuery2 = () => {
-    
+    //Make a GET call to the backend server
+    Axios.get("http://localhost:8080/api/getCountryTopTracks", {
+      //Passing in the country as the input parameter
+      params:{
+        country: input2
+      }
+    }).catch(function (error) {   //Catch any error from the response
+      console.log(error);
+    }).then( (res) => { 
+      //Adding the query response to a single string
+      var str = "";
+      res.data.forEach((entry) => {
+        str += `${JSON.stringify(entry, null, '\t')}\n`;
+      })
+      //Update the text1 state variable with the response data
+      setText2(str);
+    })
   }
 
   //Put the third query here
   const exampleQuery3 = () => {
-    
+    //Make a GET call to the backend server
+    Axios.get("http://localhost:8080/api/getUserTop", {
+      //Passing in the country as the input parameter
+      params:{
+        userId: input3
+      }
+    }).catch(function (error) {   //Catch any error from the response
+      console.log(error);
+    }).then( (res) => { 
+      //Adding the query response to a single string
+      var str = "";
+      res.data.forEach((entry) => {
+        str += `${JSON.stringify(entry, null, '\t')}\n`;
+      })
+      //Update the text1 state variable with the response data
+      setText3(str);
+    })
   }
 
   //Put the fourth query here
   const exampleQuery4 = () => {
-    
+    Axios.get("http://localhost:8080/api/insertTopTrack", {
+      //Passing in the country as the input parameter
+      params:{
+        userId: input4,
+        trackId: input5,
+        trackRank: input6
+      }
+    }).catch(function (error) {   //Catch any error from the response
+      console.log(error);
+    }).then( (res) => { 
+      //Adding the query response to a single string
+      if(res != null) {
+        var str = JSON.stringify(res.data, null, '\t');
+      }
+      //Update the text1 state variable with the response data
+      setText4("Insert Successful");
+    })
+  }
+
+  const exampleQuery5 = () => {
+    Axios.get("http://localhost:8080/api/deleteTopTrack", {
+      //Passing in the country as the input parameter
+      params:{
+        userId: input7,
+        trackId: input8,
+      }
+    }).catch(function (error) {   //Catch any error from the response
+      console.log(error);
+    }).then( (res) => { 
+      //Adding the query response to a single string
+      if(res != null) {
+        var str = JSON.stringify(res.data, null, '\t');
+      }
+      //Update the text1 state variable with the response data
+      setText5("Delete Successful");
+    })
+  }
+
+  const exampleQuery6 = () => {
+    Axios.get("http://localhost:8080/api/updateTrackRanking", {
+      //Passing in the country as the input parameter
+      params:{
+        userId: input9,
+        trackId: input10,
+        trackRank: input11
+      }
+    }).catch(function (error) {   //Catch any error from the response
+      console.log(error);
+    }).then( (res) => { 
+      //Adding the query response to a single string
+      if(res != null) {
+        var str = JSON.stringify(res.data, null, '\t');
+      }
+      //Update the text1 state variable with the response data
+      setText6("Update Successful");
+    })
   }
 
 
@@ -69,6 +171,34 @@ function App() {
   function handleInput4(event) {
     setInput4(event.target.value);
   }
+
+  function handleInput5(event) {
+    setInput5(event.target.value);
+  }
+
+  function handleInput6(event) {
+    setInput6(event.target.value);
+  }
+
+  function handleInput7(event) {
+    setInput7(event.target.value);
+  }
+
+  function handleInput8(event) {
+    setInput8(event.target.value);
+  }
+
+  function handleInput9(event) {
+    setInput9(event.target.value);
+  }
+
+  function handleInput10(event) {
+    setInput10(event.target.value);
+  }
+
+  function handleInput11(event) {
+    setInput11(event.target.value);
+  }
   
 
   return (
@@ -76,29 +206,41 @@ function App() {
       <header className="App-header">
         
         <h1>SpotiFind!</h1>
-        <h2>Query 1</h2>
-        <input onChange={handleInput1}></input>
+        <h2>Search For A Track</h2>
+        <input placeholder= "Track Name" onChange={handleInput1}></input>
         <button onClick={exampleQuery1}>Find tracks!</button>
-        <p>{input1}</p>
         <p>{text1}</p>
         
-        <h2>Query 2</h2>
-        <input onChange={handleInput2}></input>
+        <h2>Search For Top Tracks By Country</h2>
+        <input placeholder = "Country" onChange={handleInput2}></input>
         <button onClick={exampleQuery2}>Find tracks!</button>
-        <p>{input2}</p>
         <p>{text2}</p>
         
-        <h2>Query 3</h2>
-        <input onChange={handleInput3}></input>
+        <h2>Lookup your Top Tracks</h2>
+        <input placeholder = "User ID" onChange={handleInput3}></input>
         <button onClick={() => exampleQuery3()}>Find tracks!</button>
-        <p>{input3}</p>
         <p>{text3}</p>
 
-        <h2>Query 4</h2>
-        <input onChange={handleInput4}></input>
-        <button onClick={() => exampleQuery4()}>Find tracks!</button>
-        <p>{input4}</p>
+        <h2>Insert A Top Track</h2>
+        <input placeholder = "User ID" onChange={handleInput4}></input>
+        <input placeholder = "Track ID" onChange={handleInput5}></input>
+        <input placeholder = "Track Rank" onChange={handleInput6}></input>
+        <button onClick={() => exampleQuery4()}>Insert Track!</button>
         <p>{text4}</p>
+
+        <h2>Delete A Top Track</h2>
+        <input placeholder = "User ID" onChange={handleInput7}></input>
+        <input placeholder = "Track ID" onChange={handleInput8}></input>
+        <button onClick={() => exampleQuery5()}>Delete Track!</button>
+        <p>{text5}</p>
+
+        <h2>Update Track Ranking</h2>
+        <input placeholder = "User ID" onChange={handleInput9}></input>
+        <input placeholder = "Track ID" onChange={handleInput10}></input>
+        <input placeholder = "New Rank" onChange={handleInput11}></input>
+        <button onClick={() => exampleQuery6()}>Update Ranking!</button>
+        <p>{text6}</p>
+
       </header>
     </div>
   );
