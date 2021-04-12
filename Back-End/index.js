8f81c2788bb147cd6d79a5b38bb0991febcbc4db
jackdai2 <jackdai2@illinois.edu>
2021-04-12 00:11:12 -0400

    completed basic crud

--- Back-End/index.js
+++ Back-End/index.js
@@ -6,8 +6,8 @@ const bodyParser = require("body-parser");
 
 var db = mysql.createConnection({
     host: '34.121.38.95',
-    user: 'root',
-    password: 'CS411Team80',
+    user: 'jack',
+    password: 'cs411team80',
     database: 'spotifind'
 })
 
@@ -15,76 +15,61 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
-/*  EXAMPLE QUERIES (THESE DO NOT WORK)
-
-app.get("/api/getTracksByArtist", function(req, response){
-    const artist = req.query.artist;
-    console.log(req.query.artist)
-    const sqlSelect = "SELECT DISTINCT t.track_name, t.artists  \
-        FROM Artists a JOIN                                         \
-        Tracks t ON(t.artists LIKE CONCAT('%',a.artist_name, '%'))  \
-    WHERE a.artist_name = '" + artist +"' \
-    ORDER BY t.artists ASC;";
-    db.query(sqlSelect, (err, result) => {
-        console.log(err);
-        response.send(result);
-    });
-});
-
-app.post("/api/addUserTopTracks", function(req, res){
-    const artist = req.query.artist;
-    const sqlReq = `UPDATE * FROM Artists WHERE artist_name = "${artist}";`;
-    //console.log(sqlReq);
-    db.query(sqlReq, (err,result) => {
-        console.log(result);
-        
-    })
-
-    const verifyUpdate = "SELECT * FROM ...."
-    db.query(verifyUpdate, (err,result) => {
-        console.log(result);
-        res.send(result);
-    })
-} )
-
-*/
-
-app.get("/api/getArtistInfo", function(req, res){
-    // Fetch parameters from the call header
-    const artist = req.query.artist;
-    // Generate SQL call
-    const sqlReq = `SELECT * FROM Artists WHERE artist_name = "${artist}";`;
-    // Send call to the database 
-    db.query(sqlReq, (err,result) => {
-        // Send the SQL result in the API response
-        console.log(result);
-        res.send(result);
-    })
-} )
-
-
-app.put("/api/deleteUser", function(req, res){
-    const userId = req.query.id;
-    const sqlDelete = `DELETE FROM USERS WHERE user_id = "${userId}";`;
-    db.query(sqlDelete, (err, result) => {
-        res.sned(result);
-    })
-})
 
 app.get("/api/insertUser", function(req, res){
     const userId = parseInt(req.query.userId);
     const userCountry = req.query.userCountry;
-    console.log(req.query);
+    // console.log(req.query);
     const sqlInsert = `INSERT INTO Users (user_id, country, explicit) values (${userId}, "${userCountry}", 0);`;
     db.query(sqlInsert, (err, result) => {
         res.send(result);
     })
 })
 
+app.get("/api/updateUser", function(req, res){
+    const userId = parseInt(req.query.userId);
+    const userCountry = req.query.userCountry;
+    // console.log(userId);
+    // console.log(userCountry);
+
+    const sqlUpdate = `update Users set country = "${userCountry}" where user_id = ${userId};`;
+    db.query(sqlUpdate, (err, result) => {
+        res.send(result);
+    })
+})
+
+app.get("/api/deleteUser", function(req, res){
+    const userId = parseInt(req.query.userId);
+    const sqlDelete = `delete from Users where user_id = ${userId};`;
+    db.query(sqlDelete, (err, result) => {
+        res.send(result);
+    })
+})
+
+
 app.get("/api/findUser", function(req, res){
     const userId = parseInt(req.query.userId);
     //console.log(req.query);
     const sqlInsert = `SELECT * FROM Users WHERE user_id = ${userId};`;
+    db.query(sqlInsert, (err, result) => {
+        res.send(result);
+    })
+})
+app.get("/api/findHighestRankGenre", function(req, res){
+    const userId = parseInt(req.query.userId);
+    //console.log(req.query);
+    const sqlInsert = `Select genre_id, u.user_id
+                        From Users u join Top_Genres tg on u.user_id = tg.user_id 
+                        Where tg.genre_rank = ( 
+                                        Select max(tg_in.genre_rank)
+                                        From Users u_in join Top_Genres tg_in 
+                                                    on u_in.user_id = tg_in.user_id 
+                                        where u_in.user_id = u.user_id
+                                        Group By u_in.user_id
+                                        ) and 
+                                u.user_id = ${userId}
+                        Order By user_id;`
+    ;
     db.query(sqlInsert, (err, result) => {
         res.send(result);
     })
--- Front-End/src/App.js
+++ Front-End/src/App.js
@@ -12,17 +12,26 @@ function App() {
   const [input2, setInput2] = useState("");
   const [input3, setInput3] = useState("");
   const [input4, setInput4] = useState("");
-  const [text1, setText1] = useState("No query.")
-  const [text2, setText2] = useState("No query.")
-  const [text3, setText3] = useState("No query.")
-  const [text4, setText4] = useState("No query.")
+  const [text1, setText1] = useState("Insert")
+  const [text2, setText2] = useState("Update")
+  const [text3, setText3] = useState("Delete")
+  const [text4, setText4] = useState("Search")
+  const [text5, setText5] = useState("Advanced Query")
+
   const [token, setToken] = useState();
 
 
   const [userId1, setUserId1] = useState("");
   const [userCountry1, setUserCountry1] = useState("");
+
+  const [userId2, setUserId2] = useState("");
+  const [userCountry2, setUserCountry2] = useState("");
+
+  const [userId3, setUserId3] = useState("");
+
   const [userId4, setUserId4] = useState("");
 
+  const [userId5, setUserId5] = useState("");
 
   useEffect(() => {
     const hash = getTokenFromUrl();
@@ -36,25 +45,25 @@ function App() {
     console.log("token", token);
   }, []);
 
-  const exampleQuery1 = () => {
+  // const exampleQuery1 = () => {
     
-    //Make a GET call to the backend server
-    Axios.get("http://localhost:8080/api/getArtistInfo", {
-      //Passing in the artist as the input parameter
-      params:{
-        artist: input1
-      }
-    }).catch(function (error) {   //Catch any error from the response
-      console.log(error);
-    }).then( (res) => { 
-      //Adding the query response to a single string
-      if(res != null) {
-      var str = JSON.stringify(res.data, null, '\t');
-      }
-      //Update the text1 state variable with the response data
-      setText1(str);
-    })
-  }
+  //   //Make a GET call to the backend server
+  //   Axios.get("http://localhost:8080/api/getArtistInfo", {
+  //     //Passing in the artist as the input parameter
+  //     params:{
+  //       artist: input1
+  //     }
+  //   }).catch(function (error) {   //Catch any error from the response
+  //     console.log(error);
+  //   }).then( (res) => { 
+  //     //Adding the query response to a single string
+  //     if(res != null) {
+  //     var str = JSON.stringify(res.data, null, '\t');
+  //     }
+  //     //Update the text1 state variable with the response data
+  //     setText1(str);
+  //   })
+  // }
 
   const insertUser = () => {
     console.log(userId1);
@@ -76,10 +85,51 @@ function App() {
     })
   }
 
+  const updateUser = () => {
+    console.log(userId2);
+    console.log(userCountry2);
+    Axios.get("http://localhost:8080/api/updateUser", {
+      //Passing in the artist as the input parameter
+        params: {userId: userId2,
+        userCountry: userCountry2
+      }
+    }).catch(function (error) {   //Catch any error from the response
+      console.log(error);
+    }).then( (res) => { 
+      //Adding the query response to a single string
+      if(res != null) {
+      var str = JSON.stringify(res.data, null, '\t');
+      }
+      //Update the text1 state variable with the response data
+      setText2("Update Successful");
+    })
+  }
+
+
+  const deleteUser = () => {
+    console.log(userId3)
+    Axios.get("http://localhost:8080/api/deleteUser", {
+       params: {
+         userId: userId3
+       }
+   }).catch(function (error) {   //Catch any error from the response
+     console.log(error);
+   }).then( (res) => { 
+     //Adding the query response to a single string
+     var str;
+     if(res != null) {
+       str = JSON.stringify(res.data, null, '\t');
+     }
+     //Update the text1 state variable with the response data
+     setText3('Delete successful');
+   })
+ }
+
+
   const findUser = () => {
    
      Axios.get("http://localhost:8080/api/findUser", {
-      //Passing in the artist as the input parameter
+  
         params: {
           userId: userId4
         
@@ -93,25 +143,30 @@ function App() {
         str = JSON.stringify(res.data, null, '\t');
       }
       //Update the text1 state variable with the response data
-      setText2(str);
+      setText4(str);
     })
   }
 
-  //Put the second query here
-  const exampleQuery2 = () => {
-    
-  }
-
-  //Put the third query here
-  const exampleQuery3 = () => {
-    
-  }
-
-  //Put the fourth query here
-  const exampleQuery4 = () => {
-    
-  }
-
+  const findHighestRankGenre = () => {
+   
+    Axios.get("http://localhost:8080/api/findHighestRankGenre", {
+ 
+       params: {
+         userId: userId5
+       
+       }
+   }).catch(function (error) {   //Catch any error from the response
+     console.log(error);
+   }).then( (res) => { 
+     //Adding the query response to a single string
+     var str;
+     if(res != null) {
+       str = JSON.stringify(res.data, null, '\t');
+     }
+     //Update the text1 state variable with the response data
+     setText5(str);
+   })
+ }
 
   /* SETS THE INPUT STATE VARIABLES WHEN THE USER TYPES */
   function handleInput1(event) {
@@ -141,42 +196,33 @@ function App() {
 
         <h2>User Controls</h2>
         {/* User id for insert */}
-        <p>User Id</p>
-
+        <p>User Id and User Country</p>
         <input onChange={(event) => setUserId1(event.target.value)}></input>
-        <p>User country</p>
         <input onChange={(event) => setUserCountry1(event.target.value)}></input>
         <button onClick={insertUser}>Insert User</button>
         <p>{text1}</p>
+
         {/* User id for update */}
-        <input onChange={handleInput1}></input>
-        <input onChange={handleInput1}></input>
-        <button onClick={exampleQuery1}>Update User</button>
+        <input onChange={(event) => setUserId2(event.target.value)}></input>
+        <input onChange={(event) => setUserCountry2(event.target.value)}></input>
+        <button onClick={updateUser}>Update User Country</button>
+        <p>{text2}</p>
+
         {/* User id for delete */}
-        <input onChange={handleInput1}></input>
-        <button onClick={exampleQuery1}>Delete User</button>
+        <input onChange={(event) => setUserId3(event.target.value)}></input>
+        <button onClick={deleteUser}>Delete User</button>
+        <p>{text3}</p>
+
         {/* User id for search */}
         <input onChange={(event) => setUserId4(event.target.value)}></input>
         <button onClick={findUser}>Search User</button>
-        <p>{text2}</p>
-        
-        <h2>Query 2</h2>
-        <input onChange={handleInput2}></input>
-        <button onClick={exampleQuery2}>Find tracks!</button>
-        <p>{input2}</p>
-        <p>{text2}</p>
-        
-        <h2>Query 3</h2>
-        <input onChange={handleInput3}></input>
-        <button onClick={() => exampleQuery3()}>Find tracks!</button>
-        <p>{input3}</p>
-        <p>{text3}</p>
-
-        <h2>Query 4</h2>
-        <input onChange={handleInput4}></input>
-        <button onClick={() => exampleQuery4()}>Find tracks!</button>
-        <p>{input4}</p>
         <p>{text4}</p>
+        
+        {/* advanced query */}
+        <input onChange={(event) => setUserId5(event.target.value)}></input>
+        <button onClick={findHighestRankGenre}>Highest Rank Genre</button>
+        <p>{text5}</p>
+
       </header>
     </div>
   );
